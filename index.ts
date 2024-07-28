60a4b07e59ae948df693a8d9f5850b0af028cd12
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-07-28 11:41:58 +0700

    feat: js call api frankfurter

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('Hello TypeScript')
+//console.log('Hello TypeScript')
+
+const api = 'api.frankfurter.app';
+const endpoint = 'latest';
+const baseUrl = `https://${api}/${endpoint}`;
+
+fetch(`${baseUrl}?amount=100&from=THB&to=USD`)
+    .then(res => res.json())
+    .then(console.log)
